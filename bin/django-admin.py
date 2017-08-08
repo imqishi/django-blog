58bfab5e190d7c6a10bf770be16f993d3617c0cd
qishi <imqishi@hotmail.com>
2017-08-08 22:04:54 +0800

    init project.

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/root/pyEnvironments/blog/bin/python3.4
+# EASY-INSTALL-SCRIPT: 'Django==1.11.4','django-admin.py'
+__requires__ = 'Django==1.11.4'
+__import__('pkg_resources').run_script('Django==1.11.4', 'django-admin.py')
